fd6b03ed36810271ab26ee1f0ef36b827900d101
Pietro Grandi <pietro.grandi@betsson.com>
2016-05-20 14:26:06 +0200

    feature(4)

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,11 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Hero } from './hero.model';
 import { HEROES } from './mock-heroes';
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class HeroService {
-    
+
     getHeroes() {
         return Promise.resolve(HEROES);
+    }
+
+    getHeroesSlowly() {
+        return new Promise<Hero[]>( resolve =>
+            setTimeout(() => resolve(HEROES), 2000)
+        );
+    }
+
+    getHero(id: number) {
+        return Promise.resolve(HEROES).then(
+            heroes => heroes.filter(hero => hero.id === id)[0]
+        );
     }
 
 }
